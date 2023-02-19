3c95dede7a6cc677ae1d1200c5dc1cb63f870bdb
Zekai Lin <zekailin00@gmail.com>
2023-02-19 02:12:53 -0800

    vulkan camera in progress

--- renderer/camera.h
+++ renderer/camera.h
@@ -5,20 +5,26 @@
 namespace renderer
 {
 
-struct CameraBuildInfo
+struct CameraProperties
 {
-    bool IsPerspective = true;
-    int Background = 0;
-    int placeholdes = 0;
+    bool      UseFrameExtent = true;
+    glm::vec2 Extent         = {1920, 1080};
+    float     Fov            = 45.0f;
+    float     ZNear          = 0.1f;
+    float     ZFar           = 100.0f;
 };
 
 class Camera
 {
 
 public:
-    virtual glm::vec2 GetViewSize() = 0;
-    virtual void SetViewSize(glm::vec2) = 0;
+    virtual CameraProperties* GetCamProperties() = 0;
 
+    Camera() = default;
+    virtual ~Camera() = default;
+
+    Camera(const Camera&) = delete;
+    Camera& operator=(const Camera&) = delete;
 };
 
 } // namespace renderer
--- vulkan_renderer/vulkan_camera.h
+++ vulkan_renderer/vulkan_camera.h
@@ -1,16 +1,30 @@
-#pragma once 
+#pragma once
+
+#include "camera.h"
 
 #include "vulkan_texture.h"
 #include "vk_primitives/vulkan_uniform.h"
-#include "pipeline_inputs.h"
+#include "vk_primitives/vulkan_device.h"
+#include "vulkan_swapchain.h"
 
 #include <vulkan/vulkan.h>
 #include <glm/glm.hpp>
 
-class VulkanCamera
+#include <memory>
+
+namespace renderer
+{
+
+struct ViewProjection
+{
+    glm::mat4 view;
+    glm::mat4 projection;
+};
+
+class VulkanCamera: public Camera
 {
 public:
-    glm::vec2 extent;
+    static std::unique_ptr<VulkanCamera> BuildCamera(CameraProperties&);
 
     void Initialize(glm::vec2 extent, VkFormat vkFormat);
     void Destroy();
@@ -32,4 +46,11 @@ private:
     VkImageView stencilImageView{VK_NULL_HANDLE};
 
     VkFramebuffer framebuffer{VK_NULL_HANDLE};
+
+    CameraProperties properties{};
+    VulkanDevice* vulkanDevice = nullptr;
+    IVulkanSwapchain* swapchain = nullptr;
 };
+
+
+} // namespace renderer
