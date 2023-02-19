e33f95043cdbf6de5e1e67dd1cfe5179a2bab7ed
zekailin00 <zekailin00@gmail.com>
2023-02-18 23:36:59 -0800

    Camera APIs in progress

--- renderer/camera.h
+++ renderer/camera.h
@@ -1,12 +1,24 @@
 #pragma once
 
+#include <glm/vec2.hpp>
 
 namespace renderer
 {
 
+struct CameraBuildInfo
+{
+    bool IsPerspective = true;
+    int Background = 0;
+    int placeholdes = 0;
+};
+
 class Camera
 {
-    
+
+public:
+    virtual glm::vec2 GetViewSize() = 0;
+    virtual void SetViewSize(glm::vec2) = 0;
+
 };
 
 } // namespace renderer
