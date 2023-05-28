5c7edf244db3f3537a649c5bc62e60f741926131
zekailin00 <zekailin00@gmail.com>
2023-05-27 21:00:09 -0700

    compile command

---
+++ compile.sh
@@ -0,0 +1,24 @@
+
+cd /Users/zekailin00/Git/Vulkan-Renderer/resources/vulkan_shaders/Phong
+glslc shader.frag -o frag.spv
+glslc shader.vert -o vert.spv
+
+cd /Users/zekailin00/Git/Vulkan-Renderer/resources/vulkan_shaders/quad
+glslc shader.frag -o frag.spv
+glslc shader.vert -o vert.spv
+
+cd /Users/zekailin00/Git/Vulkan-Renderer/resources/vulkan_shaders/skybox
+glslc shader.frag -o frag.spv
+glslc shader.vert -o vert.spv
+
+cd /Users/zekailin00/Git/Vulkan-Renderer/resources/vulkan_shaders/wire
+glslc shader.frag -o frag.spv
+glslc shader.vert -o vert.spv
+
+cd /Users/zekailin00/Git/Vulkan-Renderer/resources/vulkan_shaders/imgui
+glslc shader.frag -o frag.spv
+glslc shader.vert -o vert.spv
+
+
+cd /Users/zekailin00/Git/Vulkan-Renderer
+rm -rf build
