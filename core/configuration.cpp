526c6a04e6cc3c638d18c1bf755a5ec0d0561e67
zekailin00 <zekailin00@gmail.com>
2023-06-08 18:24:30 -0700

    config utility

--- core/configuration.cpp
+++ core/configuration.cpp
@@ -1,3 +1,21 @@
 #include "configuration.h"
 
-std::string Configuration::workspacePath = "";
+bool Configuration::Get(std::string key, std::string& value)
+{
+    std::map<std::string, std::string>::iterator it;
+
+    if ((it = configList.find(key)) == configList.end())
+    {
+        return false;
+    }
+
+    value = it->second;
+    return true;
+}
+
+void Configuration::Set(std::string key, std::string value)
+{
+    configList[key] = value;
+}
+
+std::map<std::string, std::string> Configuration::configList{};
--- core/configuration.h
+++ core/configuration.h
@@ -1,9 +1,14 @@
 #pragma once
 
 #include <string>
+#include <map>
+
 
 class Configuration
 {
 public:
-    static std::string workspacePath;
+    static bool Get(std::string key, std::string& value);
+    static void Set(std::string key, std::string value);
+private:
+    static std::map<std::string, std::string> configList;
 };
