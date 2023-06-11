b17c067e33b2eb1ea3af861804f1d53b82234c3c
zekailin00 <zekailin00@gmail.com>
2023-06-10 23:34:33 -0700

    serialization test

--- samples/scene3.cpp
+++ samples/scene3.cpp
@@ -5,9 +5,6 @@
 #include <glm/ext.hpp>
 
 
-using namespace renderer;
-
-
 void UItest1()
 {
     ImGui::Text("test test test test");
@@ -24,20 +21,23 @@ void UItest2()
 
 class TestApp: public Application
 {
-    std::shared_ptr<Camera> camera;
-    std::shared_ptr<Mesh> mesh_test;
-    std::shared_ptr<Mesh> mesh_monster;
+public:
+    std::shared_ptr<renderer::Camera> camera;
+    std::shared_ptr<renderer::Mesh> mesh_test;
+    std::shared_ptr<renderer::Mesh> mesh_monster;
 
-    std::shared_ptr<Material> material_uv_test;
-    std::shared_ptr<Material> material_color;
+    std::shared_ptr<renderer::Material> material_uv_test;
+    std::shared_ptr<renderer::Material> material_color;
 
-    std::shared_ptr<Texture> rough;
-    std::shared_ptr<Texture> metal;
+    std::shared_ptr<renderer::Texture> rough;
+    std::shared_ptr<renderer::Texture> metal;
 
     Entity* arrowEntity;
-
+    TestApp(std::string workspacePath)
+        :Application(workspacePath){}
 
     Scene* m_scene;
+    AssetManager* m_manager;
 
     void OnCreated() override;
     void OnUpdated(float ts) override;
@@ -47,272 +47,107 @@ class TestApp: public Application
 
 void TestApp::OnCreated()
 {
-    m_scene = GetScene();
-    Entity* entity;
-
-    glm::mat4 view;
+    m_manager = GetAssetManager();
 
     // {
-    //     GltfModel model;
-    //     model.LoadModel("/Users/zekai/Desktop/rusk.gltf");
-    //     node = std::make_unique<VulkanNode>();
-    //     nodePtr = root->AddChildNode(std::move(node));
-    //     rusk = nodePtr->AddChildNode(model.GetNode());
-
-    //     view = glm::scale(glm::mat4(1.0f), glm::vec3(1.0f, 1.0f, 1.0f));
-    //     view = glm::rotate(view, 0.0f, glm::vec3(1.0f, 0.0f, 0.0f));
-    //     view = glm::rotate(view, 0.0f, glm::vec3(0.0f, 1.0f, 0.0f));
-    //     view = glm::translate(view, glm::vec3(0.0f, -0.3f, 0.3f));
-    //     nodePtr->SetTransform(view);
+    //     std::string workspace;
+    //     Configuration::Get(CONFIG_WORKSPACE_PATH, workspace);
+    //     SetActiveScene(
+    //         Scene::LoadFromFile(
+    //             workspace + "/" + SCENE_PATH + "/" +
+    //             "newScene" + SCENE_EXTENSION, Scene::State::Editor
+    //         )
+    //     );
     // }
-  
+
     {
-        entity = m_scene->NewEntity();
-        BuildMeshInfo meshInfo{};
-        std::string path = "/Users/zekai/Desktop/quad.obj";
-        renderer::ObjLoader(path, meshInfo.vertices, meshInfo.indices);
-        std::shared_ptr<Mesh> uiMesh = VulkanMesh::BuildMesh(meshInfo);
 
-        UIBuildInfo info{};
-        info.extent = {720, 720};
-        info.imgui = UItest1;
+        SetActiveScene(Scene::NewScene("newScene"));
+        m_scene = GetActiveScene();
 
-        std::shared_ptr<UI> ui = VulkanUI::BuildUI(info);
-        std::shared_ptr<Texture> uiTexture = ui->GetTexture();
+        Entity* entity = m_manager->ImportModelObj(
+            "C:/Users/zekai/Desktop/quad.obj", m_scene);
 
-        MaterialProperties matProp{};
-        matProp.albedoTexture = uiTexture;
-        std::shared_ptr<Material> uiMaterial = VulkanMaterial::BuildMaterial(&matProp);
-        uiMesh->AddMaterial(uiMaterial);//FIXME:
+        std::shared_ptr<renderer::Mesh> mesh =
+            ((renderer::MeshComponent*)entity->GetComponent(
+                Component::Type::Mesh))->mesh;
 
-        UIComponent* uiComp = (UIComponent*)entity->AddComponent(Component::Type::UI);
-        MeshComponent* meshComp = (MeshComponent*)entity->AddComponent(Component::Type::Mesh);
+        std::shared_ptr<renderer::Material> material =
+            m_manager->NewMaterial();
+        std::shared_ptr<renderer::Texture> texture =
+            m_manager->ImportTexture("C:/Users/zekai/Desktop/ws-test/img1.jpg");
+        material->AddAlbedoTexture(texture);
+        
+        mesh->AddMaterial(material);
 
-        meshComp->mesh = std::dynamic_pointer_cast<VulkanMesh>(uiMesh);
-        uiComp->ui = std::dynamic_pointer_cast<VulkanUI>(ui);
-
-        view = glm::translate(glm::mat4(1.0f), glm::vec3(2.0f, 3.0f, 0.5f)) * 
+        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(2.0f, 3.0f, 0.5f)) * 
                glm::scale(glm::mat4(1.0f), glm::vec3(2.0f, 2.0f, 1.0f));
         entity->SetLocalTransform(view);
     }
 
-    // {
-    //     node = std::make_unique<VulkanNode>();
-    //     BuildMeshInfo meshInfo{};
-    //     std::string path = "/Users/zekai/Desktop/quad.obj";
-    //     renderer::ObjLoader(path, meshInfo.vertices, meshInfo.indices);
-    //     std::shared_ptr<Mesh> uiMesh = VulkanMesh::BuildMesh(meshInfo);
-
-    //     UIBuildInfo info{};
-    //     info.extent = {720, 720};
-    //     info.imgui = UItest1;
-
-    //     std::shared_ptr<UI> ui = VulkanUI::BuildUI(info);
-    //     std::shared_ptr<Texture> uiTexture = ui->GetTexture();
-
-    //     MaterialProperties matProp{};
-    //     matProp.albedoTexture = uiTexture;
-    //     std::shared_ptr<Material> uiMat = VulkanMaterial::BuildMaterial(&matProp);
-
-    //     uiMesh->AddMaterial(uiMat);
-    //     node->ui = ui;
-    //     node->SetMesh(uiMesh);
-    //     nodePtr = root->AddChildNode(std::move(node));
-
-    //     view = glm::translate(glm::mat4(1.0f), glm::vec3(2.0f, 0.0f, -1.0f)) * 
-    //            glm::rotate(view, 3.14f, glm::vec3(0.0f, 1.0f, 0.0f)) *
-    //            glm::scale(glm::mat4(1.0f), glm::vec3(0.5f, 0.5f, 1.0f));
-    //     nodePtr->SetTransform(view);
-    // }
-
-
-    // {
-    //     entity = m_scene->NewEntity();
-    //     BuildMeshInfo meshInfo{};
-    //     std::string path = "/Users/zekai/Desktop/quad.obj";
-    //     renderer::ObjLoader(path, meshInfo.vertices, meshInfo.indices);
-    //     std::shared_ptr<Mesh> uiMesh = VulkanMesh::BuildMesh(meshInfo);
-
-    //     UIBuildInfo info{};
-    //     info.extent = {720, 360};
-    //     info.imgui = UItest2;
-
-    //     std::shared_ptr<UI> ui = VulkanUI::BuildUI(info);
-    //     std::shared_ptr<Texture> uiTexture = ui->GetTexture();
-
-    //     MaterialProperties matProp{};
-    //     matProp.albedoTexture = uiTexture;
-    //     std::shared_ptr<Material> uiMat = VulkanMaterial::BuildMaterial(&matProp);
-
-    //     uiMesh->AddMaterial(uiMat);
-    //     node->ui = ui;
-    //     node->SetMesh(uiMesh);
-    //     nodePtr = root->AddChildNode(std::move(node));
-
-
-    //     view = glm::translate(glm::mat4(1.0f), glm::vec3(-2.0f, 3.0f, -0.5f)) * 
-    //            glm::scale(glm::mat4(1.0f), glm::vec3(2.0f, 1.0f, 1.0f));
-    //     nodePtr->SetTransform(view);
-    // }
-
     {
 
-        ((WireframeComponent*)m_scene->NewEntity()->AddComponent(
+        ((renderer::WireframeComponent*)m_scene->NewEntity()->AddComponent(
             Component::Type::Wireframe))->wireframe = 
-            VulkanWireframeGenerator::GetCircle(
+            renderer::VulkanWireframeGenerator::GetCircle(
                 glm::vec3(1, 3, -1), glm::vec3(0,0.5,1), 1, {1.0, 0, 0});
 
-        ((WireframeComponent*)m_scene->NewEntity()->AddComponent(
+        ((renderer::WireframeComponent*)m_scene->NewEntity()->AddComponent(
             Component::Type::Wireframe))->wireframe = 
-            VulkanWireframeGenerator::GetCircle(
+            renderer::VulkanWireframeGenerator::GetCircle(
                 glm::vec3(1, 4, 1), glm::vec3(0,1,1), 0.5, {0, 1.0, 0});
 
-        ((WireframeComponent*)m_scene->NewEntity()->AddComponent(
+        ((renderer::WireframeComponent*)m_scene->NewEntity()->AddComponent(
             Component::Type::Wireframe))->wireframe = 
-            VulkanWireframeGenerator::GetCircle(
+            renderer::VulkanWireframeGenerator::GetCircle(
                 glm::vec3(-1, 3, 1), glm::vec3(0,4,1), 1.5, {0, 0, 1.0});
 
-        ((WireframeComponent*)m_scene->NewEntity()->AddComponent(
+        ((renderer::WireframeComponent*)m_scene->NewEntity()->AddComponent(
             Component::Type::Wireframe))->wireframe = 
-            VulkanWireframeGenerator::GetCircle(
-                glm::vec3(-1, 3, -1), glm::vec3(0,10,1), 1, {1.0, 1.0, 0});
-        
-
-        // root->AddChildNode(generator->GetLine({2, -4, 0}, {2, 3, 0}));
-        // root->AddChildNode(generator->GetLine({2, 3, 0}, {-2, 3, 0}));
-        // root->AddChildNode(generator->GetLine({-2, 3, 0}, {-2, -4, 0}));
-
-        // root->AddChildNode(generator->GetLine({-2,  2, 0}, {2,  2, 0}, {1,0,0}));
-        // root->AddChildNode(generator->GetLine({-2,  1, 0}, {2,  1, 0}, {0,1,0}));
-        // root->AddChildNode(generator->GetLine({-2,  0, 0}, {2,  0, 0}, {0,0,1}));
-        // root->AddChildNode(generator->GetLine({-2, -4, 0}, {2, -4, 0}, {1,0,0}));
-
-        // root->AddChildNode(generator->GetSphere({2,1,2}, 1, {0,1,1}));
-        // root->AddChildNode(generator->GetAABB({2,2,2}, {3,5,1}));
-        // root->AddChildNode(generator->GetOBB(glm::mat4(1.0), {1,1,0}));
-
-        // root->AddChildNode(generator->GetAABB({-1,4,0}, {1,6,0}));
-
-        
+            renderer::VulkanWireframeGenerator::GetCircle(
+                glm::vec3(-1, 3, -1), glm::vec3(0,10,1), 1, {1.0, 1.0, 0});    
     }
 
     {
-        entity = m_scene->NewEntity();
+        Entity* entity = m_scene->NewEntity();
 
-        CameraProperties prop{};
+        renderer::CameraProperties prop{};
         prop.UseFrameExtent = false;
-        this->camera = VulkanCamera::BuildCamera(prop);
+        this->camera = renderer::VulkanCamera::BuildCamera(prop);
 
-        CameraComponent* camComp =
-            (CameraComponent*)entity->AddComponent(Component::Type::Camera);
+        renderer::CameraComponent* camComp =
+            (renderer::CameraComponent*)entity->AddComponent(Component::Type::Camera);
 
-        camComp->camera = std::dynamic_pointer_cast<VulkanCamera>(this->camera);
+        camComp->camera = std::dynamic_pointer_cast<renderer::VulkanCamera>(this->camera);
 
-        view = glm::rotate(glm::mat4(1.0f), 0.0f, glm::vec3(1.0f, 0.0f, 0.0f))
+        glm::mat4 view = glm::rotate(glm::mat4(1.0f), 0.0f, glm::vec3(1.0f, 0.0f, 0.0f))
                 * glm::rotate(glm::mat4(1.0f), 0.0f, glm::vec3(0.0f, 1.0f, 0.0f))
                 * glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 3.0f, 10));
         entity->SetLocalTransform(view);
     }
 
-    // {
-    //     node = std::make_unique<VulkanNode>();
-    //     nodePtr = root->AddChildNode(std::move(node));
-
-    //     nodePtr->SetCamera(VulkanVrDisplay::BuildCamera());
-
-    //     view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 5.0f, 3.0));
-    //     nodePtr->SetTransform(view);
-    //     vrCamNode = nodePtr;
-    // }
-
-    // {
-    //     node = std::make_unique<VulkanNode>();
-    //     nodePtr = vrCamNode->AddChildNode(std::move(node));
-
-    //     BuildMeshInfo meshInfo{};
-    //     std::string path = "resources/models/debug/arrow_debug.obj";
-    //     renderer::ObjLoader(path, meshInfo.vertices, meshInfo.indices);
-    //     mesh_test = VulkanMesh::BuildMesh(meshInfo);
-
-    //     nodePtr->SetMesh(mesh_test);
-
-    //     view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, -2.0f, 0));
-    //     nodePtr->SetTransform(view);
-    // }
-
-
     {
-        entity = m_scene->NewEntity();
+        Entity* entity = m_manager->ImportModelObj(
+            "C:/Users/zekai/Desktop/Vulkan-Renderer/resources/models/debug/arrow_debug.obj",
+            m_scene
+        );
 
-        BuildMeshInfo meshInfo{};
-        std::string path = "resources/models/debug/arrow_debug.obj";
-        renderer::ObjLoader(path, meshInfo.vertices, meshInfo.indices);
-        mesh_test = VulkanMesh::BuildMesh(meshInfo);
+        std::shared_ptr<renderer::Mesh> mesh =
+            ((renderer::MeshComponent*)entity->GetComponent(
+                Component::Type::Mesh))->mesh;
 
-        MaterialProperties matProp{};
-        matProp.albedo = {1, 0, 0};
-        material_uv_test = VulkanMaterial::BuildMaterial(&matProp);
-        mesh_test->AddMaterial(material_uv_test);
+        std::shared_ptr<renderer::Material> material =
+            m_manager->NewMaterial();
+        material->SetAlbedo({1, 0, 0});
+        mesh->AddMaterial(material);
 
-        MeshComponent* meshComp =
-            (MeshComponent*)entity->AddComponent(Component::Type::Mesh);
-        meshComp->mesh = std::dynamic_pointer_cast<VulkanMesh>(mesh_test);
-
-        LightComponent* lightComp =
-            (LightComponent*)entity->AddComponent(Component::Type::Light);
+        renderer::LightComponent* lightComp =
+            (renderer::LightComponent*)entity->AddComponent(Component::Type::Light);
         lightComp->properties.color = {10, 5, 5};
 
-        meshComp->mesh = std::dynamic_pointer_cast<VulkanMesh>(mesh_test);
         arrowEntity = entity;
+        entity->name = "arrow";
     }
-
-    // {
-    //     node = std::make_unique<VulkanNode>();
-    //     nodePtr = root->AddChildNode(std::move(node));
-
-    //     renderer::LightProperties prop{};
-    //     prop.color = {5, 5, 5};
-    //     prop.type = DIRECTIONAL_LIGHT;
-    //     std::shared_ptr<Light> light = VulkanLight::BuildLight(prop);
-
-    //     nodePtr->SetLight(light);
-
-    //     view = glm::rotate(glm::mat4(1.0f), 1.5f, glm::vec3(1.0f, 0.0f, 0.0f));
-    //     view = glm::rotate(view, 0.0f, glm::vec3(0.0f, 1.0f, 0.0f));
-    //     nodePtr->SetTransform(view);
-    //     lightNode = nodePtr;
-    // }
-
-    // {
-    //     node = std::make_unique<VulkanNode>();
-    //     nodePtr = root->AddChildNode(std::move(node));
-
-    //     renderer::LightProperties prop{};
-    //     prop.color = {20, 5, 5};
-    //     prop.type = DIRECTIONAL_LIGHT;
-    //     std::shared_ptr<Light> light = VulkanLight::BuildLight(prop);
-
-    //     nodePtr->SetLight(light);
-    //     lightNode2 = nodePtr;
-    // }
-
-    // {
-    //     node = std::make_unique<VulkanNode>();
-    //     nodePtr = root->AddChildNode(std::move(node));
-
-    //     BuildMeshInfo meshInfo{};
-    //     std::string path = "resources/models/debug/arrow_debug.obj";
-    //     renderer::ObjLoader(path, meshInfo.vertices, meshInfo.indices);
-    //     mesh_test = VulkanMesh::BuildMesh(meshInfo);
-
-    //     nodePtr->SetMesh(mesh_test);
-    //     MaterialProperties matProp{};
-    //     matProp.albedo = {0, 0, 1};
-    //     material_uv_test = VulkanMaterial::BuildMaterial(&matProp);
-    //     mesh_test->AddMaterial(material_uv_test);
-
-    //     lightDebug2 = nodePtr;
-    // }
 }
 
 void TestApp::OnUpdated(float ts)
@@ -325,33 +160,27 @@ void TestApp::OnUpdated(float ts)
 
     view = glm::rotate(glm::mat4(1.0f), totalTime/1.0f, glm::vec3(1.0f, 0.0f, 0.0f));
 
-    arrowEntity->SetLocalTransform(view);
-    // lightDebug->SetTransform(view);
-
-    // view = glm::rotate(glm::mat4(1.0f), 0.7f, glm::vec3(1.0f, 0.0f, 0.0f));
-    // view = glm::rotate(view, totalTime/1.0f + 3.14f, glm::vec3(0.0f, 0.0f, 1.0f));
-    // lightNode2->SetTransform(view);
-    // lightDebug2->SetTransform(view);
-
-    // view = glm::rotate(glm::mat4(1.0f), totalTime/3.0f, glm::vec3(0.0f, 1.0f, 0.0f));
-    // //rusk->SetTransform(view);
-
-    // view = glm::rotate(glm::mat4(1.0f), 0/3.0f, glm::vec3(1.0f, 0.0f, 0.0f))
-    //             * glm::rotate(glm::mat4(1.0f), totalTime/3.0f, glm::vec3(0.0f, 1.0f, 0.0f))
-    //             * glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 3.0f, 10));
-    // camNode->SetTransform(view);
-
+    Entity* arrow = GetActiveScene()->GetEntityByName("arrow");
+    arrow->SetLocalTransform(view);
 }
 
 void TestApp::OnDestroy()
 {
+    Scene* scene = GetActiveScene();
+    std::string workspace;
+    Configuration::Get(CONFIG_WORKSPACE_PATH, workspace);
 
+    scene->SaveToFile(
+        workspace + "/" + SCENE_PATH + "/" +
+        scene->GetSceneName() + SCENE_EXTENSION
+    );
+    delete scene;
 }
 
 int main(int argv, char** argc)
 {
     {   
-        TestApp app{};
+        TestApp app("C:\\Users\\zekai\\Desktop\\ws-test\\test2");
         app.Run();
     }
 }
